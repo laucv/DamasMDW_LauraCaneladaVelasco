4984ef40f91388d45569a1162dbb6ea42cac894b
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-04-22 19:05:26 +0200

    Add testRemovingRandomPieceAfterNotEatingWithWhitePieces and testRemovingRandomPieceAfterNotEatingWithBlackPieces

--- src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/EatingPieceAfterOpponentNotEatingPiece.java
+++ src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/EatingPieceAfterOpponentNotEatingPiece.java
@@ -1,4 +1,66 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
-public class EatingPieceAfterOpponentNotEatingPiece {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class EatingPieceAfterOpponentNotEatingPiece extends GameTest{
+
+    private void assertMove(Coordinate... coordinates){
+        assertNull(this.game.move(coordinates));
+        assertEquals(this.game, this.expectedGame);
+    }
+
+    @Test
+    void testRemovingRandomPieceAfterNotEatingWithWhitePieces(){
+        this.setGame(Color.WHITE,
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            " n      ",
+            "  b     ",
+            "        ",
+            "        ");
+        this.setExpectedGame(Color.BLACK,
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            " n      ",
+            "        ",
+            "        ",
+            "        ");
+        this.assertMove(
+            new Coordinate(5, 2),
+            new Coordinate(4, 3)
+        );
+    }
+
+    @Test
+    void testRemovingRandomPieceAfterNotEatingWithBlackPieces(){
+        this.setGame(Color.WHITE,
+            "        ",
+            "        ",
+            "        ",
+            "    n   ",
+            "   b    ",
+            "        ",
+            "        ",
+            "        ");
+        this.setExpectedGame(Color.BLACK,
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "   b    ",
+            "        ",
+            "        ",
+            "        ");
+        this.assertMove(
+            new Coordinate(3, 4),
+            new Coordinate(4, 5)
+        );
+    }
 }
